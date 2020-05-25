2dc96872ef5490da80d931c5dd1f9c3909afdcdd
JacquelineMZ <jacqueline.mndez@gmail.com>
2020-05-25 16:31:56 -0500

    Login

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const server = express();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root@localhost:3307/delilah_resto');
@@ -76,6 +77,54 @@ server.post('/usuario/registrar',(req,res)=>{
         res.json({error_presentado:error});
     });
 });
+// {
+// 	"ID_USUARIO":"3",
+// 	"USUARIO":"CRIS",
+// 	"CONTRASENA_USUARIO":"1234",
+// 	"NOMBRES":"Cristian",
+// 	"APELLIDOS":"Vega",
+// 	"CORREO":"cristian.vega@gmail.com",
+// 	"DIRECCION":"calle verdadera 123",
+// 	"CELULAR":"3204777515",
+// 	"ID_ROL":"1"
+// }
+
+server.post('/usuario/login',(req,res)=>{
+    const {USUARIO,CONTRASENA_USUARIO} = req.body;
+    const validado = validarUsuarioContrasena(USUARIO,CONTRASENA_USUARIO);
+    if(!validado){
+        res.status(401);
+        res.json({error:'Usuario o contraseña erroneos'});
+        return;
+    }
+    const token = jwt.sign({usuario},firma);
+    // console.log(usuario);
+    res.json({token});
+    console.log(token);
+});
+
+async function validarUsuarioContrasena(USUARIOF,CONTRASENA_USUARIOF){
+    var respuesta;
+    sequelize.query('SELECT * FROM USUARIOS WHERE USUARIO=:USUARIO AND CONTRASENA_USUARIO=:CONTRASENA_USUARIO', 
+    { replacements: { 
+        USUARIO:USUARIOF, 
+        CONTRASENA_USUARIO:CONTRASENA_USUARIOF }
+    , type: sequelize.QueryTypes.SELECT }
+    ).then( function (resultados){
+        if(resultados[0].ID_USUARIO != undefined)
+        respuesta = true;
+        else 
+        respuesta = false;
+    }).catch(function (error) {
+        respuesta = false;
+    });
+    return respuesta;
+}
+// {
+// 	"USUARIO":"CRIS",
+// 	"CONTRASENA_USUARIO":"1234"
+// }
+
 
 server.listen(3000,()=>{
     console.log("Server corriendo");
