c95d2c36483068e911076a4d7df7583735454385
JacquelineMZ <jacqueline.mndez@gmail.com>
2020-05-25 15:21:31 -0500

    Post de insercion de usuarios Ok

--- endpoints.js
+++ endpoints.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize('mysql://root@localhost:3307/delilah_resto');
 
 server.use(express.json());
 //Endpoints 
-// GET un solo usuario Swagger /usuario/listauno/
+// GET un solo usuario Swagger /usuario/listauno/ Parametro idfiltro
 server.get('/usuario/listauno/:idfiltro',(req,res)=>{
     const filtro = req.params.idfiltro;
     sequelize.query('SELECT * FROM USUARIOS where ID_USUARIO = :filtro limit 1', 
@@ -21,6 +21,55 @@ server.get('/usuario/listatodos/',(req,res)=>{
     const filtro = req.params.idfiltro;
     sequelize.query('SELECT * FROM USUARIOS', 
     { type: sequelize.QueryTypes.SELECT }
+    ).then(resultados => res.json(resultados)
+    ).catch(function (error) {
+        res.status(401);
+        res.json({error_presentado:error});
+    });
+});
+//POST de registro usuario /usuario/registrar
+server.post('/usuario/registrar',(req,res)=>{
+    const {
+        ID_USUARIO,
+        USUARIO,
+        CONTRASENA_USUARIO,
+        NOMBRES,
+        APELLIDOS,
+        CORREO,
+        DIRECCION,
+        CELULAR,
+        ID_ROL
+    } = req.body
+    sequelize.query('INSERT INTO USUARIOS (\
+            ID_USUARIO,\
+            USUARIO,\
+            CONTRASENA_USUARIO,\
+            NOMBRES,\
+            APELLIDOS,\
+            CORREO,\
+            DIRECCION,\
+            CELULAR,\
+            ID_ROL\
+        )VALUES (\
+            :ID_USUARIO, \
+            :USUARIO, \
+            :CONTRASENA_USUARIO, \
+            :NOMBRES, \
+            :APELLIDOS, \
+            :CORREO, \
+            :DIRECCION, \
+            :CELULAR, \
+            :ID_ROL)', 
+    { replacements: { 
+        ID_USUARIO:ID_USUARIO, 
+        USUARIO:USUARIO, 
+        CONTRASENA_USUARIO:CONTRASENA_USUARIO, 
+        NOMBRES:NOMBRES, 
+        APELLIDOS:APELLIDOS,
+        CORREO:CORREO,
+        DIRECCION:DIRECCION,
+        CELULAR:CELULAR,
+        ID_ROL:ID_ROL,}}
     ).then(resultados => res.json(resultados)
     ).catch(function (error) {
         res.status(401);
