66a66790dfbd102952b194a6c119d61ede04a5b0
JacquelineMZ <jacqueline.mndez@gmail.com>
2020-05-25 14:51:06 -0500

    Endpoints get de usuarios OK

--- endpoints.js
+++ endpoints.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root@localhost:3307/delilah_resto');
 
 server.use(express.json());
-//Endpoint GET Usuarios
+//Endpoints 
+// GET un solo usuario Swagger /usuario/listauno/
 server.get('/usuario/listauno/:idfiltro',(req,res)=>{
     const filtro = req.params.idfiltro;
     sequelize.query('SELECT * FROM USUARIOS where ID_USUARIO = :filtro limit 1', 
@@ -15,36 +16,17 @@ server.get('/usuario/listauno/:idfiltro',(req,res)=>{
         res.json({error_presentado:error});
     });
 });
-
-// INSERT INTO ROLES (
-//         ID_ROL,
-//         DESCRIPCION
-//     ) 
-//     VALUES (
-//     1,
-//     'Administrador'
-//     );
-// INSERT INTO USUARIOS (
-//         ID_USUARIO,
-//         USUARIO,
-//         CONTRASENA_USUARIO,
-//         NOMBRES,
-//         APELLIDOS,
-//         CORREO,
-//         DIRECCION,
-//         CELULAR,
-//         ID_ROL
-//     ) VALUES (
-//     1,
-//     'JACQUIE',
-//     '123',
-//     'Jacqueline',
-//     'Mendez Zualuaga',
-//     'jacqueline.mndez@gmail.com',
-//     'Calle falsa 123',
-//     '3192065788',
-//     1
-//     );
+// GET un todos los usuarios Swagger /usuario/listatodos/
+server.get('/usuario/listatodos/',(req,res)=>{
+    const filtro = req.params.idfiltro;
+    sequelize.query('SELECT * FROM USUARIOS', 
+    { type: sequelize.QueryTypes.SELECT }
+    ).then(resultados => res.json(resultados)
+    ).catch(function (error) {
+        res.status(401);
+        res.json({error_presentado:error});
+    });
+});
 
 server.listen(3000,()=>{
     console.log("Server corriendo");
