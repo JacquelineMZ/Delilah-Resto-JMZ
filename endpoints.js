55144fbad74554f3b288abe48ab855bd0b3451c2
JacquelineMZ <jacqueline.mndez@gmail.com>
2020-05-25 13:07:53 -0500

    inicio de script, falta iniciar node

--- endpoints.js
+++ endpoints.js
@@ -1,1 +1,8 @@
-var first = 3;
+const express = require('express');
+const server = express();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root@localhost:3307/delilah_resto');
+
+server.listen(3000,()=>{
+    console.log("Server corriendo");
+});
