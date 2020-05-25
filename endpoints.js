6164d1cc631fa71d5da5073ae0c4b04c888f64d8
JacquelineMZ <jacqueline.mndez@gmail.com>
2020-05-25 14:37:26 -0500

    Query para la insercion de un usuario

--- endpoints.js
+++ endpoints.js
@@ -16,6 +16,36 @@ server.get('/usuario/listauno/:idfiltro',(req,res)=>{
     });
 });
 
+// INSERT INTO ROLES (
+//         ID_ROL,
+//         DESCRIPCION
+//     ) 
+//     VALUES (
+//     1,
+//     'Administrador'
+//     );
+// INSERT INTO USUARIOS (
+//         ID_USUARIO,
+//         USUARIO,
+//         CONTRASENA_USUARIO,
+//         NOMBRES,
+//         APELLIDOS,
+//         CORREO,
+//         DIRECCION,
+//         CELULAR,
+//         ID_ROL
+//     ) VALUES (
+//     1,
+//     'JACQUIE',
+//     '123',
+//     'Jacqueline',
+//     'Mendez Zualuaga',
+//     'jacqueline.mndez@gmail.com',
+//     'Calle falsa 123',
+//     '3192065788',
+//     1
+//     );
+
 server.listen(3000,()=>{
     console.log("Server corriendo");
 });
